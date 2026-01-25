acd12756cdfc363865525c313e99ef3d099d8839
Rafael Trindade <rafatrindade.exe@gmail.com>
2026-01-25 16:13:21 -0300

    feat(infra): provisiona bucket de objetos e configuração s3-compatible

---
+++ terraform/storage.tf
@@ -0,0 +1,14 @@
+# ==============================================================================
+# SQUAD 3 - DATA LAKE STORAGE STRATEGY (OBJECT STORAGE)
+# Objetivo: Provisionar o Bucket para suporte à Arquitetura Medallion
+# ==============================================================================
+
+resource "oci_objectstorage_bucket" "lake_squad3" {
+  compartment_id = var.compartment_id
+  name           = "lake-squad3"
+  namespace      = var.os_namespace
+  storage_tier   = "Standard"
+  versioning     = "Enabled"
+  access_type    = "NoPublicAccess"
+  object_events_enabled = true
+}
